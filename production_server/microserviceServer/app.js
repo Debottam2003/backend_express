0b7b61f9cb6687b37c967c4ba530ea053666a389
Debottam Kar <debottamkar2003@gmail.com>
2025-07-25 18:31:36 +0530

    production grade microservice server added

---
+++ production_server/microserviceServer/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import router from './routes/userroutes.js'
+// import ejs from 'ejs';
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/mydatabase';
+// console.log("Loaded MONGO_URI:", process.env.MONGO_URI);
+
+app.use(cors());
+app.set('view engine', 'ejs');
+
+app.use('/api', router);
+
+async function dbconnect() {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log("DataBase connected.");
+        app.listen(PORT, () => {
+            console.log("The server is running on port:", PORT);
+        });
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+dbconnect();
---
+++ production_server/microserviceServer/controllers/usercontroller.js
@@ -0,0 +1,33 @@
+import users from '../database/usermodel.js'
+
+export let getuser = async function (req, res) {
+    try {
+        let userdata = await users.find();
+        res.status(200).render('index.ejs', { users: userdata });
+        // res.status(200).json({ users: userdata });
+        // console.log(userdata);
+    } catch (error) {
+        res.status(500).send({ error: "An error occurred while fetching user data." });
+    }
+};
+
+export let setuser = async function (req, res) {
+    console.log(req.body);
+    try {
+        let { username, age, shopping } = req.body;
+        // Optional: Handle missing fields
+        if (!username || !age || !shopping) {
+            return res.status(400).send({ error: "All fields are required." });
+        }
+        await users.create({
+            username,
+            age,
+            shopping: Number(shopping),
+        });
+        // res.status(200).send("New User Data added successfully.");
+        res.redirect('/api/getuser'); // Redirect to the getuser route to display updated data
+        return;
+    } catch (error) {
+        return res.status(500).send({ error: "An error occurred while adding user data." });
+    }
+};
