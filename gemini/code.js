251a613600817aab350e981a780d8d6c0b2bfb1b
Debottam2003 <debottamkar2003@gmail.com>
2025-08-08 20:27:43 +0530

    gemini api integrate

---
+++ gemini/code.js
@@ -0,0 +1,33 @@
+import { GoogleGenAI, Modality } from "@google/genai";
+import * as fs from "node:fs";
+
+async function main() {
+
+    const ai = new GoogleGenAI({ apiKey: "AIzaSyCLferB1sy4g9fC-6UiXJjbaKzEhKDA5ZI" });
+
+    const contents =
+        "biriyani food image";
+
+    // Set responseModalities to include "Image" so the model can generate  an image
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash-preview-image-generation",
+        contents: contents,
+        config: {
+            responseModalities: [Modality.TEXT, Modality.IMAGE],
+        },
+    });
+    console.log(response.candidates[0].content.parts);
+    for (const part of response.candidates[0].content.parts) {
+        // Based on the part type, either show the text or save the image
+        if (part.text) {
+            console.log(part.text);
+        } else if (part.inlineData) {
+            const imageData = part.inlineData.data;
+            const buffer = Buffer.from(imageData, "base64");
+            fs.writeFileSync(`${Date.now()}.png`, buffer);
+            console.log("Image saved as gemini-native-image.png");
+        }
+    }
+}
+
+main();
