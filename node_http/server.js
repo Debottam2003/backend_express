2e152ab308ad0304e1b6722b51a1816a9dec9b3d
Debottam2003 <debottamkar2003@gmail.com>
2025-08-31 03:02:25 +0530

    concepts about port 80 and http is added

--- node_http/server.js
+++ node_http/server.js
@@ -5,14 +5,15 @@ let server = http.createServer(function (req, res) {
 
     // Normalize URL by checking if it starts with /newpage
     if (req.url === "/newpage" && req.method === 'GET') {
-        res.end('Welcome');
+        res.end('Welcome To http server');
     } else {
-        res.statusCode = 404;  // Set the response status code to 404 (Not Found)
+        // res.statusCode = 404;  // Set the response status code to 404 (Not Found)
+        res.writeHead(404, { "Content-Type": "text/html" });
         res.end('404 Not Found');  // Send a 404 response to the client
         console.log("No such URL");
     }
 });
 
-server.listen(3333, () => {
-    console.log("Server is running on port 3003...");
+server.listen(80, () => {
+    console.log("Server is running on port 80(Default Port)...");
 });
