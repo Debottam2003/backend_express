5821581cb4258a8fe05f3055605efd0bcd1d856b
Debottam2003 <debottamkar2003@gmail.com>
2025-11-04 14:02:13 +0530

    fixes

--- express/mysql/db.js
+++ express/mysql/db.js
@@ -1,14 +1,21 @@
 import pool from 'mysql2/promise';
 
-const msPool = pool.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'okudera2003',
-    database: 'deb',
-    connectionLimit: 100,
-});
+// const msPool = pool.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'debottam',
+//     connectionLimit: 100,
+// });
 
-let [data] = await msPool.query("select * from user limit 1");
+const msPool = pool.createPool(
+    'mysql://root:@localhost:3306/debottam',
+    {
+        connectionLimit: 100
+    }
+);
+
+let [data] = await msPool.query("select * from users;");
 console.log(data);
 
 export default msPool;
--- express/postgresql/db.js
+++ express/postgresql/db.js
@@ -2,13 +2,18 @@ import pkg from 'pg';
 
 const { Pool } = pkg;
 
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'search',
+//     password: 'okudera2003',
+//     port: 3000,
+//     max: 100
+// });
+
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'Search',
-    password: 'okudera2003',
-    port: 3000,
-    max: 100
+    connectionString: "postgresql://postgres:okudera2003@localhost:3000/search",
+    max: 100,
 });
 
 let { rows } = await pool.query("select * from admins limit 1");
