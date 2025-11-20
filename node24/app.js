51fde8b483f7aac251293195d53e9269c6143880
Debottam Kar <debottamkar2003@gmail.com>
2025-11-20 18:01:37 +0530

    new features

--- node24/app.js
+++ node24/app.js
@@ -1,2 +1,5 @@
 console.log(process.env.PORT);
 console.log(process.env.SECRET_KEY);
+// No need of nodemon --warch flag is here
+// TS support is also added(Partially)
+// Just run the app using the command: node --watch --env-file=.env app.js 
