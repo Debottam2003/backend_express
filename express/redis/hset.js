c877afcb59361615eea918b409a8ed48cec45e5a
Debottam2003 <debottamkar2003@gmail.com>
2026-05-22 16:29:08 +0530

    notes on redis

--- express/redis/hset.js
+++ express/redis/hset.js
@@ -11,10 +11,13 @@ await redisClient.connect();
 await redisClient.hSet("user:100", {
   name: "John Doe",
   email: "john.doe@example.com",
-  age: "30",
+  age: 30,
 });
 
 let userData = await redisClient.hGetAll("user:100");
 console.log(userData);
+console.log("User Name:", userData.name);
+console.log("User Email:", userData.email);
+console.log("User Age:", Number(userData.age));
 
 await redisClient.quit();
